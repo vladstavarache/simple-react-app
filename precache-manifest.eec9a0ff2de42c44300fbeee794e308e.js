e43969e31484f389f494abbf602e45b55a211bc5
Vlad Stavarache <vlad.stavarache@ro.ibm.com>
2020-04-11 21:52:47 +0300

    Updates

---
+++ precache-manifest.eec9a0ff2de42c44300fbeee794e308e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79438f35d18257611f3bdbdbcbf99a2",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "918937b5ddd7245069f9",
+    "url": "/simple-react-app/static/css/main.e47e008d.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/simple-react-app/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918937b5ddd7245069f9",
+    "url": "/simple-react-app/static/js/main.ca952790.chunk.js"
+  },
+  {
+    "revision": "31c37b957b06ef2c4067",
+    "url": "/simple-react-app/static/js/runtime-main.5ccc0ae7.js"
+  }
+]);
